dd36a30c15dbfe7e5e9544cd7479dfc6daa85509
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-05-30 20:07:18 +0530

    Added Basic Structure to the Website

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
@@ -7,10 +8,19 @@ module.exports = {
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      fontFamily: {
+        "Mansalva": ['Mansalva', 'cursive'],
+        "Space-Grotesk": ['Space Grotesk', 'sans-serif'],
+        "Space-Mono": ['Space Mono', 'monospace'],
+        "Hurricane": ['Hurricane', 'cursive'],
+        "Poppins": ['Poppins', 'sans-serif'],
+      },
+      colors: {
+        'nav-blue': "#3EA9F2"
+      },
+      opacity: {
+        "33": ".33",
+        "66": ".66",
       },
     },
   },
