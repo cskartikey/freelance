9ed2ec642acb3bf1917311f331c316cdadb6be2f
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-05-31 18:29:56 +0530

    Added Background and Colours

---
+++ pages/contact/index.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import Head from 'next/head';
+
+const Contact : NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Freelance : Contact</title>
+            </Head>
+        </>
+    )
+}
+
+export default Contact;
---
+++ pages/team/index.tsx
@@ -0,0 +1,14 @@
+import { type NextPage } from "next";
+import Head from 'next/head';
+const Team : NextPage = () => {
+    return (
+        <>
+            <Head>
+                <title>Freelance : Team</title>
+            </Head>
+          
+        </>
+    )
+}
+
+export default Team;
