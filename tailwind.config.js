a3866310ee37884cfcaf7aa359c6d78c5c749096
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-05-31 15:26:00 +0530

    Background and Colour Change + About dir change

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
   ],
   theme: {
     extend: {
+      backgroundImage: {
+        'blue-triange-pattern': "url('../assets/svg/blue-triangle.svg')",
+        'purp-triange-pattern': "url('../assets/svg/purp-rectangle.svg')",
+      },
       fontFamily: {
         "Mansalva": ['Mansalva', 'cursive'],
         "Space-Grotesk": ['Space Grotesk', 'sans-serif'],
@@ -17,6 +21,7 @@ module.exports = {
       colors: {
         'nav-blue': "#3EA9F2",
         "scroll-blue" : "#BBE1FA",
+        "rectangle-purp" :"#9376E0",
       },
       opacity: {
         "33": ".33",
