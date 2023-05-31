27b1afac64a6cfa591dcb1406b46e410968bf355
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-05-31 15:26:00 +0530

    Background and Colour Change + About dir change

--- components/Nav.tsx
+++ components/Nav.tsx
@@ -1,15 +1,36 @@
-export default function Nav(){
-    return (
-        <nav className='flex justify-between pt-6 px-12'>
-        <div> 
-          <p className='font-Mansalva text-white text-3xl cursor-pointer'><span className='font-Space-Grotesk text-nav-blue'>&lt;</span>Freelance<span className='font-Space-Grotesk text-nav-blue'>/&gt;</span></p>
-        </div>
-        <div className='space-x-8 pt-4'>
-          <span className='font-Space-Grotesk-Bold text-white font-bold text-sm cursor-pointer'>HOME</span>
-          <span className='font-Space-Grotesk-Bold text-white font-bold text-sm opacity-33 hover:opacity-66 cursor-pointer' >ABOUT</span>
-          <span className='font-Space-Grotesk-Bold text-white font-bold text-sm opacity-33 hover:opacity-66 cursor-pointer' >TEAM</span>
-          <span className='font-Space-Grotesk-Bold text-white font-bold text-sm opacity-33 hover:opacity-66 cursor-pointer'>CONTACT</span>
-        </div>
-      </nav>
-    )
+import { useRouter } from "next/router";
+import { useState, useEffect } from "react";
+
+export default function Nav() {
+  const router = useRouter();
+  const [navColor, setColor] = useState("text-nav-blue");
+  useEffect(() => { 
+    if(router.pathname === "/about"){
+      setColor("text-rectangle-purp"); 
+    }
+  }, [router.pathname]);
+  return (
+    <nav className={`flex justify-between pt-6 px-12 ${navColor}`}>
+      <div>
+        <p className="font-Mansalva text-white text-3xl cursor-pointer">
+          <span className={`font-Space-Grotesk ${navColor}`}>&lt;</span>Freelance
+          <span className={`font-Space-Grotesk ${navColor}`}>/&gt;</span>
+        </p>
+      </div>
+      <div className="space-x-8 pt-4">
+        <span className="font-Space-Grotesk-Bold text-white font-bold text-sm cursor-pointer">
+          HOME
+        </span>
+        <span className="font-Space-Grotesk-Bold text-white font-bold text-sm opacity-33 hover:opacity-66 cursor-pointer">
+          ABOUT
+        </span>
+        <span className="font-Space-Grotesk-Bold text-white font-bold text-sm opacity-33 hover:opacity-66 cursor-pointer">
+          TEAM
+        </span>
+        <span className="font-Space-Grotesk-Bold text-white font-bold text-sm opacity-33 hover:opacity-66 cursor-pointer">
+          CONTACT
+        </span>
+      </div>
+    </nav>
+  );
 }
--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,18 @@
 import '@/css/globals.css'
 import Layout from '@/layouts/layout';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/router';
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+  const [backgroundImage, setBackgroundImage] = useState("bg-blue-triange-pattern");
+  useEffect(() => { 
+    if(router.pathname === "/about"){
+      setBackgroundImage("bg-purp-triange-pattern"); 
+    }
+  }, [router.pathname]);
+  useEffect(() => {
+    document.body.classList.add(backgroundImage);
+  });
   return (
     <Layout>
       <Component {...pageProps} />
