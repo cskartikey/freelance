bd09894d3a1e7ceb80aced3ebeda4fc457027bf2
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2023-06-01 08:08:56 +0000

    added typing effect

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,12 +1,27 @@
 import { type NextPage } from "next";
 import Head from 'next/head';
+import Typewriter from "typewriter-effect";
+
 const Team : NextPage = () => {
     return (
         <>
             <Head>
                 <title>Team</title>
             </Head>
-          
+            <section>
+                <div>
+                    <Typewriter
+                      onInit={(typewriter) => {
+                        typewriter.typeString('Hi there!')
+                          .start();
+                      }}
+                      options={{
+                        wrapperClassName: 'team-typewriter-text-wrapper',
+                        cursorClassName: 'team-typewriter-cursor-wrapper Typewriter__cursor',
+                      }}
+                    />
+                </div>
+            </section>
         </>
     )
 }
