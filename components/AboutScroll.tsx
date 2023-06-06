6b4ade87bd54d89a70a7dcf0872ef2523f64a096
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-06-07 02:57:07 +0530

    Shifted code for rectangle animation to apt file

--- components/AboutScroll.tsx
+++ components/AboutScroll.tsx
@@ -1,45 +1,25 @@
-import React, { useRef, useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/dist/ScrollTrigger";
-import { Power2 } from "gsap";
-import AboutUS from "@/pages/about";
-import AboutUS_2 from "@/pages/about/2";
-import AboutUS_3 from "@/pages/about/3";
+import React, { useRef, useEffect } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { Power2 } from 'gsap'
+import AboutUS from '@/pages/about'
+import AboutUS_2 from '@/pages/about/2'
+import AboutUS_3 from '@/pages/about/3'
 
 function AboutScroll() {
-  const sectionRef = useRef(null);
-  const triggerRef = useRef(null);
+  const sectionRef = useRef(null)
+  const triggerRef = useRef(null)
 
-  gsap.registerPlugin(ScrollTrigger);
+  gsap.registerPlugin(ScrollTrigger)
 
   useEffect(() => {
-    let scrollArr = gsap.utils.toArray(".scroll");
-    let tl = gsap.timeline({defaults:{ duration: 1, ease: Power2.easeInOut}})
-    tl.to(".shape-l", {x:-40, y:-20})
-    .to(".shape-r", {x:40, y:20}, "<")
-    .to(".shape-r-2", {x:40, y:30}, "<")
-    tl.progress(1)
-    let tk = gsap.to(".hero", {
-      scrollTrigger:{
-        start: "top top",
-        trigger: ".hero",
-        markers: true,
-        onEnter: function () {
-          if(tl.progress() < 1){
-            tl.play()
-          }
-        },
-        onEnterBack: function () {
-          tl.reverse()
-          tl.progress(0)
-        },
-      },
-    })
+    let scrollArr = gsap.utils.toArray('.scroll')
+
     let to = gsap.to(scrollArr, {
       xPercent: () => -100 * (scrollArr.length - 1),
       ease: Power2.easeInOut,
       scrollTrigger: {
-        start: "top top",
+        start: 'top top',
         trigger: sectionRef.current,
         markers: false,
         pin: true,
@@ -47,33 +27,27 @@ function AboutScroll() {
         scrub: 0.8,
         invalidateOnRefresh: true,
         anticipatePin: 1,
-        snap: 0.5 / (scrollArr.length - 1),
+        snap: 1 / (scrollArr.length - 1),
         onEnter: function () {
-          const element = document.getElementsByClassName("nav-t");
+          const element = document.getElementsByClassName('nav-t')
 
           for (let i = 0; i < element.length; i++) {
-            element[i].classList.replace(
-              "text-nav-blue",
-              "text-rectangle-purp"
-            );
+            element[i].classList.replace('text-nav-blue', 'text-rectangle-purp')
           }
         },
         onLeaveBack() {
-          const element = document.getElementsByClassName("nav-t");
+          const element = document.getElementsByClassName('nav-t')
           for (let i = 0; i < element.length; i++) {
-            element[i].classList.replace(
-              "text-rectangle-purp",
-              "text-nav-blue"
-            );
+            element[i].classList.replace('text-rectangle-purp', 'text-nav-blue')
           }
         },
-        end: () => "+=" + window.innerWidth,
+        end: () => '+=' + window.innerWidth,
       },
-    });
+    })
     return () => {
-      to.kill();
-    };
-  }, []);
+      to.kill()
+    }
+  }, [])
 
   return (
     <section className="overflow-hidden" id="about-scroll">
@@ -100,7 +74,7 @@ function AboutScroll() {
         </div>
       </div>
     </section>
-  );
+  )
 }
 
-export default AboutScroll;
+export default AboutScroll
--- components/Hero.tsx
+++ components/Hero.tsx
@@ -1,38 +1,68 @@
-import Image from "next/image";
-import image from "assets/svg/Image.svg";
-import Head from "next/head";
+import Image from 'next/image'
+import image from 'assets/svg/Image.svg'
+import Head from 'next/head'
+import React, { useEffect } from 'react'
+import { gsap } from 'gsap'
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger'
+import { Power2 } from 'gsap'
 
 export default function Hero() {
-  return(
+  gsap.registerPlugin(ScrollTrigger)
+
+  useEffect(() => {
+    let tl = gsap.timeline({
+      defaults: { duration: 2, ease: Power2.easeInOut },
+    })
+    tl.to('.shape-l', { x: -40, y: -20 })
+    tl.to('.shape-r', { x: 40, y: 20 }, '<')
+    tl.to('.shape-r-2', { x: 40, y: 30 }, '<')
+
+    gsap.to('.hero', {
+      scrollTrigger: {
+        start: 'top top',
+        trigger: '.hero',
+        markers: true,
+        onEnter: function () {
+          if (tl.progress() < 1) {
+            tl.play()
+          }
+        },
+        onLeaveBack: function () {
+          tl.reverse(0)
+        },
+      },
+    })
+  })
+  return (
     <main className="hero flex text-5xl items-center h-screen justify-center">
-    <Head>
-      <title>Freelance</title>
-    </Head>
-    <section>
-      <div className="pr-40 mr-96 mb-36">
-        <div className="absolute inset-0 flex justify-center items-center z-10">
-          <Image src={image} alt="" />
+      <Head>
+        <title>Freelance</title>
+      </Head>
+      <section>
+        <div className="pr-40 mr-96 mb-36">
+          <div className="absolute inset-0 flex justify-center items-center z-10">
+            <Image src={image} alt="" />
+          </div>
+          <div className="flex flex-col border-nav-blue space-y-4 border-l h-full pl-5 ">
+            <span className="font-Hurricane text-white text-8xl z-20">
+              Designing
+            </span>
+            <span className="font-Poppins text-white z-20">and</span>
+            <span className="font-Space-Mono text-white z-20">
+              <span className="opacity-50">&lt;</span>developing
+              <span className="opacity-50">/&gt;</span>
+            </span>
+            <span className="font-Poppins text-white z-20">
+              websites for <span className="text-nav-blue">business</span>
+            </span>
+          </div>
         </div>
-        <div className="flex flex-col border-nav-blue space-y-4 border-l h-full pl-5 ">
-          <span className="font-Hurricane text-white text-8xl z-20">
-            Designing
-          </span>
-          <span className="font-Poppins text-white z-20">and</span>
-          <span className="font-Space-Mono text-white z-20">
-            <span className="opacity-50">&lt;</span>developing
-            <span className="opacity-50">/&gt;</span>
-          </span>
-          <span className="font-Poppins text-white z-20">
-            websites for <span className="text-nav-blue">business</span>
-          </span>
-        </div>
+      </section>
+      <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[65vw] h-[85vh] max-w-[1000px] max-h-[600px]">
+        <div className="shape-l absolute rounded-md border-nav-blue border-solid border-2 opacity-50 w-3/6 h-80 top-0 mt-4 left-0 ml-10 z-0"></div>
+        <div className="shape-r absolute rounded-md border-nav-blue border-solid border-2 opacity-50 w-5/6 h-5/6 bottom-0 right-0 z-0"></div>
+        <div className="shape-r-2 absolute rounded-md border-nav-blue border-solid border-2 opacity-50 w-3/6 h-3/6 -bottom-6 -right-8 z-0"></div>
       </div>
-    </section>
-    <div className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 w-[65vw] h-[85vh] max-w-[1000px] max-h-[600px]">
-      <div className="shape-l absolute rounded-md border-nav-blue border-solid border-2 opacity-50 w-3/6 h-80 top-0 mt-4 left-0 ml-10 z-0"></div>
-      <div className="shape-r absolute rounded-md border-nav-blue border-solid border-2 opacity-50 w-5/6 h-5/6 bottom-0 right-0 z-0"></div>
-      <div className="shape-r-2 absolute rounded-md border-nav-blue border-solid border-2 opacity-50 w-3/6 h-3/6 -bottom-6 -right-8 z-0"></div>
-    </div>
-  </main>
-  );
+    </main>
+  )
 }
