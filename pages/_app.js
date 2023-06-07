a92c62067e430dce208de6de17f2e9f921a08106
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-06-08 00:58:02 +0530

    Removed redudant code

--- pages/_app.js
+++ pages/_app.js
@@ -3,28 +3,10 @@ import Layout from '@/layouts/layout'
 import { useEffect, useState } from 'react'
 import { useRouter } from 'next/router'
 function MyApp({ Component, pageProps }) {
-  const router = useRouter()
-  const [backgroundImage, setBackgroundImage] = useState(
-    'bg-blue-triange-pattern'
-  )
-  useEffect(() => {
-    if (router.pathname === '/') {
-      setBackgroundImage('bg-blue-triange-pattern')
-    } else if (router.pathname.includes('/about')) {
-      setBackgroundImage('bg-purp-rectangle-pattern')
-    } else if (router.pathname.includes('/team')) {
-      setBackgroundImage('bg-green-triange-pattern')
-    } else if (router.pathname.includes('/contact')) {
-      setBackgroundImage('bg-red-rectangle-pattern')
-    }
-  }, [router.pathname])
   useEffect(() => {
     const body = document.body
-    body.classList.forEach((item) => {
-      body.classList.remove(item)
-    })
-    body.classList.add(backgroundImage)
-  }, [backgroundImage])
+    body.classList.add('wrapper')
+  })
   return (
     <Layout>
       <Component {...pageProps} />
