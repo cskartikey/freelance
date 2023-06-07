1f1ca4a6b279b83a01e9afb03aa96f73a4f8f008
Thearcane <cvxanonymous007@gmail.com>
2023-06-07 07:24:21 +0000

    routes added

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,8 +4,28 @@ import TeamScroll from '@/components/TeamScroll'
 import ContactScroll from '@/components/ContactScroll'
 import Nav from '@/components/Nav'
 import Head from 'next/head'
+import { useEffect } from 'react'
 
 export default function Index() {
+
+  useEffect(() => {
+    window.addEventListener('scroll', (e) => {
+      console.log(location.pathname)
+      if (scrollY <= window.innerHeight && location.pathname != '/')
+        window.history.pushState('', 'Freelance', '/')
+      else if (scrollY > window.innerHeight && scrollY <= window.innerHeight * 3 && location.pathname.includes('/about') == false)
+        window.history.pushState('', 'Freelance', '/about')
+      else if (scrollY > window.innerHeight * 3 && scrollY <= window.innerHeight * 7 && location.pathname.includes('/team') == false)
+        window.history.pushState('', 'Freelance', '/team')
+      else if (scrollY > window.innerHeight * 7 && scrollY <= window.innerHeight * 9 && location.pathname.includes('contact') == false)
+        window.history.pushState('', 'Freelance', '/contact')
+    })
+    window.addEventListener('popstate', (e) => {
+      e.preventDefault()
+      history.go(history.length - 1)
+    })
+  }, [])
+
   return (
     <>
       <Nav />
