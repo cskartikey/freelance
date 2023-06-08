011af0c52df8a22310f2434cc5bd0687d22ef856
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-06-08 12:48:27 +0530

    Removed footer DOM change

--- components/AboutScroll.tsx
+++ components/AboutScroll.tsx
@@ -41,7 +41,7 @@ function AboutScroll() {
       ease: Power2.easeInOut,
       scrollTrigger: {
         start: 'top top',
-        trigger: sectionRef.current,
+        trigger: triggerRef.current,
         markers: false,
         pin: true,
         pinSpacing: true,
@@ -50,93 +50,16 @@ function AboutScroll() {
         anticipatePin: 1,
         snap: 1 / (scrollArr.length - 1),
         onEnter: function () {
-          const element = document.getElementsByClassName('nav-t')
-          const element_footer = document.getElementsByClassName('footer')
-          const element_footer_1 = document.getElementById('footer-number-1')
-          const element_footer_2 = document.getElementById('footer-number-2')
-          const mouse = document.getElementById('mouse')
-          const rightarrow = document.getElementById('right-arrow')
-          const scrolltext = document.getElementById('scroll-text')
-          const dottedline = document.getElementById('dotted-line')
-          document.getElementsByClassName('footer-number')
-          for (let i = 0; i < element.length; i++) {
-            element[i].classList.replace('text-nav-blue', 'text-rectangle-purp')
+          const navBarText = document.getElementsByClassName('nav-t')
+          for (let i = 0; i < navBarText.length; i++) {
+            navBarText[i].classList.replace('text-nav-blue', 'text-rectangle-purp')
           }
-          for (let i = 0; i < element_footer.length; i++) {
-            element_footer[i].classList.replace(
-              'border-nav-blue',
-              'border-rectangle-purp'
-            )
-            element_footer[i].classList.replace(
-              'divide-nav-blue',
-              'divide-rectangle-purp'
-            )
-          }
-          element_footer_1?.classList.replace(
-            'bg-scroll-blue',
-            'text-transparent'
-          )
-          element_footer_2?.classList.replace(
-            'text-transparent',
-            'bg-footer-number-red'
-          )
-          mouse?.classList.replace('text-scroll-blue', 'text-footer-number-red')
-          scrolltext?.classList.replace(
-            'bg-scroll-blue',
-            'bg-footer-number-red'
-          )
-          dottedline?.classList.replace(
-            'border-scroll-blue',
-            'border-footer-number-red'
-          )
-          rightarrow?.classList.replace(
-            'text-scroll-blue',
-            'text-footer-number-red'
-          )
         },
         onLeaveBack() {
-          const element = document.getElementsByClassName('nav-t')
-          const element_footer = document.getElementsByClassName('footer')
-          const element_footer_1 = document.getElementById('footer-number-1')
-          const element_footer_2 = document.getElementById('footer-number-2')
-          const mouse = document.getElementById('mouse')
-          const rightarrow = document.getElementById('right-arrow')
-          const scrolltext = document.getElementById('scroll-text')
-          const dottedline = document.getElementById('dotted-line')
-          for (let i = 0; i < element.length; i++) {
-            element[i].classList.replace('text-rectangle-purp', 'text-nav-blue')
+          const navBarText = document.getElementsByClassName('nav-t')
+          for (let i = 0; i < navBarText.length; i++) {
+            navBarText[i].classList.replace('text-rectangle-purp', 'text-nav-blue')
           }
-          for (let i = 0; i < element_footer.length; i++) {
-            element_footer[i].classList.replace(
-              'border-rectangle-purp',
-              'border-nav-blue'
-            )
-            element_footer[i].classList.replace(
-              'divide-rectangle-purp',
-              'divide-nav-blue'
-            )
-          }
-          element_footer_1?.classList.replace(
-            'text-transparent',
-            'bg-scroll-blue'
-          )
-          element_footer_2?.classList.replace(
-            'bg-footer-number-red',
-            'text-transparent'
-          )
-          mouse?.classList.replace('text-footer-number-red', 'text-scroll-blue')
-          scrolltext?.classList.replace(
-            'bg-footer-number-red',
-            'bg-scroll-blue'
-          )
-          dottedline?.classList.replace(
-            'border-footer-number-red',
-            'border-scroll-blue'
-          )
-          rightarrow?.classList.replace(
-            'text-footer-number-red',
-            'text-scroll-blue'
-          )
         },
         end: () => '+=' + window.innerWidth,
       },
