a6f4d016f4e8c21f86ca06db0bf177d4c8bf2f8f
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-06-09 03:19:42 +0530

    Removed Redudant Code

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,6 @@
 import '@/css/globals.css'
 import Layout from '@/layouts/layout'
-import { useEffect, useState } from 'react'
-import { useRouter } from 'next/router'
 function MyApp({ Component, pageProps }) {
-  useEffect(() => {
-    const body = document.body
-    body.classList.add('wrapper')
-  })
   return (
     <Layout>
       <Component {...pageProps} />
