c47cb7609dd9f79a43549c8efbd493c093c09895
Kartikey S. Chauhan <64925909+cskartikey@users.noreply.github.com>
2023-06-08 14:06:39 +0530

    Further Scroll Work

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,18 +1,11 @@
-import { useRouter } from 'next/router'
-import { useState, useEffect } from 'react'
 import Link from 'next/link'
-import { idText } from 'typescript'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import {
-  faChevronLeft,
   faChevronRight,
   faComputerMouse,
 } from '@fortawesome/free-solid-svg-icons'
 
-export default function Footer() {
-  const router = useRouter()
-  // const [footer, setFotColor] = useState('border-nav-blue')
-  // const [footerNumber, setnFotColor] = useState('bg-scroll-blue')
+function Footer() {
   return (
     <footer className="fixed bottom-16 w-full flex justify-start pl-8 z-50">
       <div className="pt-2">
@@ -27,50 +20,33 @@ export default function Footer() {
         </Link>
       </div>
       <div
-        className={`footer grid grid-cols-4 divide-x-2 divide-opacity-50 divide-nav-blue border-solid border-2 border-nav-blue ml-36 border-opacity-50 font-Space-Mono`}
+        className={`footer grid grid-cols-4 divide-x-2 divide-opacity-50 divide-nav-blue border-solid border-2 border-nav-blue ml-36 border-opacity-50 font-Space-Mono z-50`}
       >
         <div
-          className={`bg-scroll-blue text-black opacity-90 px-2 text-lg`}
-          id="footer-number-1"
+          className={`footer-text bg-scroll-blue text-black opacity-90 px-2 text-lg`}
         >
           1
         </div>
-        <div
-          className={`footer-number-2 text-black opacity-90 px-2 text-lg text-transparent`}
-          id="footer-number-2"
-        >
-          2
+        <div className={`text-black opacity-90 px-2 text-lg text-transparent`}>
+          .
         </div>
-        <div
-          className={`footer-number-3 text-black opacity-90 px-2 text-lg text-transparent `}
-          id="footer-number-3"
-        >
-          3
+        <div className={`text-black opacity-90 px-2 text-lg text-transparent `}>
+          .
         </div>
-        <div
-          className={`footer-number-4 text-black opacity-90 px-2 text-lg text-transparent`}
-          id="footer-number-4"
-        >
-          4
+        <div className={`text-black opacity-90 px-2 text-lg text-transparent`}>
+          .
         </div>
       </div>
       <div>
         <FontAwesomeIcon
           icon={faComputerMouse}
-          className="text-scroll-blue text-3xl ml-12"
-          id="mouse"
+          className="mouse text-scroll-blue text-3xl ml-12"
         />
       </div>
       <div className="relative">
-        <div
-          className="border-0 border-t-4 h-1 w-[50vw] border-dotted mt-2 ml-4 border-scroll-blue"
-          id="dotted-line"
-        ></div>
+        <div className="dotted-line border-0 border-t-4 h-1 w-[50vw] border-dotted mt-2 ml-4 border-scroll-blue"></div>
         <div className="absolute bottom-1 right-96 flex items-center justify-center w-full h-full">
-          <div
-            className="scroll-txt text-black bg-scroll-blue rounded-md font-Space-Grotesk h-6 w-16 text-center z-50"
-            id="scroll-text"
-          >
+          <div className="scroll-text text-black bg-scroll-blue rounded-md font-Space-Grotesk h-6 w-16 text-center z-50">
             Scroll.
           </div>
         </div>
@@ -79,10 +55,11 @@ export default function Footer() {
       <div>
         <FontAwesomeIcon
           icon={faChevronRight}
-          className="text-scroll-blue mb-0.5"
-          id="right-arrow"
+          className="right-arrow text-scroll-blue mb-0.5"
         />
       </div>
     </footer>
   )
 }
+
+export default Footer
