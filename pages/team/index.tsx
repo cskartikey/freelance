567d00682d815234f028b969db7c27f4b78cd9dc
Thearcane <cvxanonymous007@gmail.com>
2023-06-08 14:13:11 +0530

    fixed redirect

--- pages/team/index.tsx
+++ pages/team/index.tsx
@@ -1,24 +1,31 @@
 import { type NextPage } from 'next'
-import Head from 'next/head'
 import Typewriter from 'typewriter-effect'
+import { useEffect, useState } from 'react'
+import { useRouter } from 'next/router'
 
 const TeamMember: NextPage = () => {
+
+  let [ typingStatus, setTypingStatus ] = useState<boolean>(false)
+  const router = useRouter()
+
+  useEffect(() => {
+    window.onscroll = () => {
+      if(scrollY > window.innerHeight * 3 - 10 && typingStatus === false)
+        setTypingStatus(true)
+    }
+    if(window.location.pathname.includes('/team'))
+      router.push('/')
+  }, [])
+
   return (
     <>
-      <Head>
-        <title>Team</title>
-      </Head>
       <section className="w-screen h-screen">
         <section className="max-w-[610px] absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 text-center min-h-[200px]">
           <Typewriter
-            onInit={(typewriter) => {
-              typewriter
-                .typeString(
-                  'Some badass tagline which basically means Meet Our Team (again Typing animation.)'
-                )
-                .start()
-            }}
             options={{
+              strings: 'Some badass tagline which basically means Meet Our Team (again Typing animation.)',
+              autoStart: typingStatus,
+              loop: false,
               wrapperClassName: 'team-typewriter-text-index',
               cursorClassName:
                 'team-typewriter-cursor-wrapper Typewriter__cursor',
