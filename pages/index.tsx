b610a5daad2b6a431a552fb27c79db9aae0de983
Thearcane <96725322+AnonymousXC@users.noreply.github.com>
2023-06-12 18:39:43 +0530

    fixed dual nav bar in dom

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,13 +2,11 @@ import Hero from '@/components/Hero'
 import AboutScroll from '@/components/AboutScroll'
 import TeamScroll from '@/components/TeamScroll'
 import ContactScroll from '@/components/ContactScroll'
-import Nav from '@/components/Nav'
 import { useEffect } from 'react'
 
 export default function Index() {
   return (
     <>
-      <Nav />
       <Hero />
       <AboutScroll />
       <TeamScroll />
